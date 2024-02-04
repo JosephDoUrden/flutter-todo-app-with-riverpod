3b057b70f4a16c2f4e7cf90c141c90182778caed
JosephDoUrden <yusufhan.sacak@bahcesehir.edu.tr>
2024-02-04 21:04:04 +0300

    Update task completion status

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -2,29 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:intl/intl.dart';
 import 'package:todo_app_riverpod/data/data.dart';
+import 'package:todo_app_riverpod/utils/utils.dart';
 
 class TaskTile extends StatelessWidget {
-  const TaskTile({super.key, required this.task});
+  const TaskTile({super.key, required this.task, this.onCompleted});
   final Task task;
+  final Function(bool?)? onCompleted;
 
   @override
   Widget build(BuildContext context) {
+    final style = context.textTheme;
+    final double iconOpacity = task.isDone ? 0.3 : 0.5;
+    final backgroundOpacity = task.isDone ? 0.1 : 0.3;
+    final textDecoration = task.isDone ? TextDecoration.lineThrough : TextDecoration.none;
+    final fontWeight = task.isDone ? FontWeight.normal : FontWeight.bold;
+
     return Row(
       children: [
         Container(
           padding: const EdgeInsets.all(9),
           decoration: BoxDecoration(
             shape: BoxShape.circle,
-            color: task.category.color.withOpacity(0.3),
+            color: task.category.color.withOpacity(backgroundOpacity),
             border: Border.all(
-              color: task.category.color,
+              color: task.category.color.withOpacity(iconOpacity),
               width: 2,
             ),
           ),
           child: Center(
             child: Icon(
               task.category.icon,
-              color: task.category.color,
+              color: task.category.color.withOpacity(iconOpacity),
             ),
           ),
         ),
@@ -33,12 +41,27 @@ class TaskTile extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(task.title),
-              Text(DateFormat.yMMMMd().format(task.date)),
+              Text(
+                task.title,
+                style: style.titleMedium?.copyWith(
+                  decoration: textDecoration,
+                  fontSize: 20,
+                  fontWeight: fontWeight,
+                ),
+              ),
+              Text(
+                DateFormat.yMMMMd().format(task.date),
+                style: style.titleMedium?.copyWith(
+                  decoration: textDecoration,
+                ),
+              ),
             ],
           ),
         ),
-        Checkbox(value: task.isDone, onChanged: (value) {})
+        Checkbox(
+          value: task.isDone,
+          onChanged: onCompleted,
+        )
       ],
     );
   }
